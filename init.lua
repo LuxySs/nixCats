656322b882e0f279807ed70d425be33c2a91b836
lulu <lucas.verbeiren@gmail.com>
2024-08-06 11:46:49 +0200

    importing plugins in a separated file

--- init.lua
+++ init.lua
@@ -7,14 +7,6 @@ if require('nixCatsUtils').isNixCats then
   -- this list literally just tells lazy.nvim not to download the plugins in the list.
   pluginList = require('nixCatsUtils.lazyCat').mergePluginTables(allPlugins.start, allPlugins.opt)
 
-  -- it wasnt detecting that these were already added
-  -- because the names are slightly different from the url.
-  -- when that happens, add them to the list, then also specify the new name in the lazySpec
-  pluginList[ [[Comment.nvim]] ] = ''
-  pluginList[ [[LuaSnip]] ] = ''
-  -- alternatively you can do it all in the plugins spec instead of modifying this list.
-  -- just set the name and then add `dev = require('nixCatsUtils').lazyAdd(false, true)` to the spec
-
   -- HINT: to view the names of all plugins downloaded via nix, use the `:NixCats pawsible` command.
 
   -- we also want to pass in lazy.nvim's path
@@ -55,39 +47,7 @@ local lazyOptions = {
 
 -- [[ Configure and install plugins ]]
 -- nixCats: this the lazy wrapper.
-require('nixCatsUtils.lazyCat').setup(pluginList, nixLazyPath, {
-  -- Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  -- nixCats: instead of uncommenting them, you can enable them
-  -- from the categories set in your packageDefinitions in your flake or other template!
-  -- This is because within them, we used nixCats to check if it should be loaded!
-
-  require 'plugins.alpha',
-  require 'plugins.autopairs',
-  require 'plugins.cmp',
-  require 'plugins.colorscheme',
-  require 'plugins.comment',
-  require 'plugins.debug',
-  require 'plugins.formatting',
-  require 'plugins.indent-blankline',
-  require 'plugins.lint',
-  require 'plugins.lsp',
-  require 'plugins.lualine',
-  require 'plugins.markdown',
-  require 'plugins.mini',
-  require 'plugins.neotree',
-  require 'plugins.oil',
-  require 'plugins.otter',
-  require 'plugins.telescope',
-  require 'plugins.todo-comments',
-  require 'plugins.treesitter',
-  require 'plugins.ufo',
-  require 'plugins.vimtex',
-  require 'plugins.whichkey',
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
-}, lazyOptions)
+require('nixCatsUtils.lazyCat').setup(pluginList, nixLazyPath, require 'plugins', lazyOptions)
 
 require 'options'
 require 'keymaps'
